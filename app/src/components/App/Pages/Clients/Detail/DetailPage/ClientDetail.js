d4b1d024877ba919771237c0c4743b7ec7a6053d
NielsMoens <nielmoen@student.arteveldehs.be>
2021-04-18 00:33:19 +0200

    :lipstick: final tweaks

--- app/src/components/App/Pages/Clients/Detail/DetailPage/ClientDetail.js
+++ app/src/components/App/Pages/Clients/Detail/DetailPage/ClientDetail.js
@@ -9,17 +9,16 @@ const ClientDetail = ({ client }) => {
         <img
           className="card-img-top"
           src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
-          alt="Card image"
-        ></img>
+          alt="Card image"/>
         <div className="card-body">
           <h4 className="card-text">{client.name} </h4>
           <p className="card-text">Email: {client.email} </p>
           <p className="card-text">Company: {client.company} </p>
           <Link
-            className="btn btn-primary"
+            className="btn btn-outline-warning"
             to={route(Routes.ClientsEdit, { id: client._id })}
           >
-            Edit client
+            ✏️ Edit client
           </Link>
         </div>
       </div>
